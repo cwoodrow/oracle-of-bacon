9ecc0a9c812d0a70d8fb156f44020f381eb6db43
Chris Woodrow <chris.woodrow@serli.com>
2017-01-14 02:48:32 +0100

    added last 10 search

--- src/components/Last10Searches.vue
+++ src/components/Last10Searches.vue
@@ -3,17 +3,8 @@
     <!-- <input :placeHolder="msg" class="actor-name"></input>
     <button class="search-button">Search</button> -->
     <h3>Last 10 searches</h3>
-    <ol>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
-      <li>Kevin Spacey</li>
+    <ol >
+      <li v-for="search in lastSearches">{{search}}</li>
     </ol>
   </div>
 </template>
@@ -22,8 +13,13 @@
   export default {
     name: 'last-10-searches',
     data() {
+      fetch('/api/last-searches')
+        .then(response => response.json())
+        .then((jsonBody) => {
+          this.lastSearches = jsonBody;
+        });
       return {
-    //    msg: 'Kevin Bacon',
+        lastSearches: [],
       };
     },
   };
